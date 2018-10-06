25bfd18d566e470ea9a1dfdb7b5665a3aa607494
cuixiaohui <2916617871@qq.com>
2018-10-06 11:42:46 +0800

    detail-header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -3,22 +3,23 @@
     <detail-banner
       
     ></detail-banner>
-    <!-- <detail-header></detail-header> -->
-    <!-- <div class="content">
-      <detail-list :list="list"></detail-list>
-    </div> -->
+    <detail-header></detail-header>
+    <div class="content">
+      <!-- <detail-list :list="list"></detail-list> -->
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
-// import DetailHeader from './components/Header'
+import DetailHeader from './components/Header'
 // import DetailList from './components/List'
 import axios from 'axios'
 export default {
   name: 'Detail',
   components:{
-      DetailBanner
+      DetailBanner,
+      DetailHeader
   }
 }
 </script>
